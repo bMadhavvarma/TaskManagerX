2e63425a043a7e59585b9ce542019a0e7743b96b
Madhav Varma <160639800+bMadhavvarma@users.noreply.github.com>
2024-11-26 19:12:46 +0530

    Update AuthProvider.jsx

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,10 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { getlocalStorage, setlocalStorage } from '../utils/LocalStorage';
-
+localStorage.clear()
 export const AuthContext = createContext();
  
 const AuthProvider = ({ children }) => {
-  setlocalStorage()
+  
     const [authData, setUserData] = useState({ employees: [], admin: null });
 
      function modifiedData(){
